6735ce390b7e94f50f7556e287c1689bd2b62cfe
Fernando Mumbach <fermuch@ayvu.net>
2024-04-17 10:11:57 -0300

    initial commit

---
+++ examples/testing/src/main.cpp
@@ -0,0 +1,24 @@
+
+// UNIX startup header
+#include <thread>
+extern void setup();
+extern void loop();
+int main() {
+  setup();
+
+  for (;;) {
+    loop();
+    std::this_thread::yield();
+  }
+}
+
+// Testing setup
+// call with: pio test
+#ifdef PIO_UNIT_TESTING
+#include <gtest/gtest.h>
+void setup() {
+  ::testing::InitGoogleTest();
+  exit(RUN_ALL_TESTS());
+}
+void loop() {}
+#endif // PIO_UNIT_TESTING
---
+++ examples/testing/test/test-message-encoding.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+class MessageEncodingTest : public ::testing::Test
+{
+// protected:
+//   void SetUp() override
+//   {
+//     // Initialize BaseSettings object
+//     baseSettings = new BaseSettings();
+//     baseSettings->initialize();
+//   }
+
+//   void TearDown() override
+//   {
+//     delete baseSettings;
+//   }
+
+//   BaseSettings* baseSettings;
+};
+
+TEST_F(MessageEncodingTest, message)
+{
+  ASSERT_EQ(true, true);
+}
